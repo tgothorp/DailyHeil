1406502512aec47670093cbfea43c1e69c74cf23
Tom <tom.gothorp@hotmail.com>
2020-05-09 16:39:30 +0100

    Configure proxy Redirector

---
+++ src/DailyHeil/Functions/Redirector.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using DailyHeil.Functions.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+
+namespace DailyHeil.Functions.Functions
+{
+    public class Redirector
+    {
+        private readonly FunctionConfiguration _functionConfiguration;
+
+        public Redirector(FunctionConfiguration functionConfiguration)
+        {
+            _functionConfiguration = functionConfiguration;
+        }
+        
+        [FunctionName("Redirector")]
+        public async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Function, "get", Route = null)]
+            HttpRequest httpRequest)
+        {
+            string path = (string) httpRequest.RouteValues["path"];
+            
+            if (string.IsNullOrEmpty(path) || path.ToLower() == "index")
+                return new RedirectResult($"{_functionConfiguration.Domain}/Index", true);
+            
+            if (path.ToLower() == "faq")
+                return new RedirectResult($"{_functionConfiguration.Domain}/Faq", true);
+            
+            return new RedirectResult($"{_functionConfiguration.Domain}/Index", true);
+        }
+    }
+}
